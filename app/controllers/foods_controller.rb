e51a51817503ef06865c14e1b82678f042d92853
Naji-97 <elbairabennaji@gmail.com>
2023-08-01 13:06:30 +0100

    add food model

--- app/controllers/foods_controller.rb
+++
@@ -1,3 +0,0 @@
-class FoodsController < ApplicationController
-  def index; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'foods#index'
+  resources :foods, except: [:update]
 end
