26aa71f1c235042611ada9f68b785449ee92c47c
Naji-97 <elbairabennaji@gmail.com>
2023-08-01 10:33:00 +0100

    Set up Devise and customize login and registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+  }
   # get 'foods/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
