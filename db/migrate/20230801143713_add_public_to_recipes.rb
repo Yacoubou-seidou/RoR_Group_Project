1e49bcfdf0bcf03d5a8033405218dbd0b2686fad
yacoubou-seidou <yacoseidou@gmail.com>
2023-08-04 14:37:43 +0100

    Fix migration

--- db/migrate/20230801143713_add_public_to_recipes.rb
+++
@@ -1,7 +0,0 @@
-class AddPublicToRecipes < ActiveRecord::Migration[7.0]
-  def change
-   add_column :recipes, :public, :boolean, default: false
-  end
-end
-
-
--- db/migrate/20230802184711_change_name_data_type_in_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNameDataTypeInUsers < ActiveRecord::Migration[7.0]
-  def change
-    change_column :users, :name, :string, limit: 255
-  end
-end
