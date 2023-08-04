acb1c0f934bd702359a041d551f0bb38a1995260
Naji-97 <elbairabennaji@gmail.com>
2023-08-04 08:49:40 +0100

    add the require changes

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods
   has_many :recipes, through: :recipe_foods, dependent: :destroy
   validates :name, presence: true
   validates :price, presence: true
